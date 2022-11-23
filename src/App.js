b0be4b74e1372c47c3f30b206752be0b9933ec3d
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-23 10:43:42 +0100

    paragraph in component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
 	return (
 		<div className="app">
 			<h1>Hi there!</h1>
-			{showParagraph && <p>Paragraph !!!!!!!</p>}
+			{<DemoOutput show={showParagraph} />}
 
 			<Button onClick={toggleShowHandler}>Toggle Paragraph ! </Button>
 		</div>
---
+++ src/components/Demo/DemoOutput.js
@@ -0,0 +1,5 @@
+function DemoOutput(props) {
+	return <p>{props.show ? 'Paragraph !!!!!!!' : ''}</p>;
+}
+
+export default DemoOutput;
