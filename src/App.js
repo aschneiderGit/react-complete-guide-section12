e0540b0eb779bdd01e4cec210c4c44d4c76c54b2
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-23 10:38:05 +0100

    Toggle paragraph

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+	const [showParagraph, setShowParagraph] = useState(false);
+	const toggleShowHandler = (event) => {
+		setShowParagraph((prevState) => !prevState);
+	};
+	return (
+		<div className="app">
+			<h1>Hi there!</h1>
+			{showParagraph && <p>Paragraph !!!!!!!</p>}
+
+			<Button onClick={toggleShowHandler}>Toggle Paragraph ! </Button>
+		</div>
+	);
 }
 
 export default App;
