a6269c9e72bf0d863d033a931df8a7fd704b04af
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-23 11:06:32 +0100

    React memo on Demo

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,7 +1,9 @@
 import MyParagraph from './MyParagraph';
+import React from 'react';
 
 function DemoOutput(props) {
+	console.log('demo test');
 	return <MyParagraph>{props.show ? 'Paragraph !!!!!!!' : ''}</MyParagraph>;
 }
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
